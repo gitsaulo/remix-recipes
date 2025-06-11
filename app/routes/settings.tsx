9869c1df14ed47293bd4b7c96fac3edd6d0b5170
gitsaulo <saulo.alcide@outlook.es>
2025-06-11 02:55:14 +0200

    casa 11/06

---
+++ app/routes/settings.tsx
@@ -0,0 +1,14 @@
+import { Link, Outlet } from "react-router";
+export default function Settings() {
+    return (
+        <div>
+            <h1>Settings</h1>
+            <p>This is the settings page</p>
+            <nav>
+                <Link to="app">App</Link>
+                <Link to="profile">Profile</Link>
+            </nav>
+            <Outlet/>
+        </div>
+    );
+}
